f8dae52b9f0ae6fcdcf61dcf566478fa97d24711
Ghulam Mustafa <mustafaji@gmail.com>
2021-10-09 22:49:06 +0200

    updated category model

--- server/models/category.js
+++ server/models/category.js
@@ -1,35 +1,48 @@
 const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema;
 
-const categorySchema = new mongoose.Schema(
+const linkSchema = new mongoose.Schema(
     {
-        name: {
+        title: {
             type: String,
             trim: true,
             required: true,
-            max: 32
+            max: 256
+        },
+        url: {
+            type: String,
+            trim: true,
+            required: true,
+            max: 256
         },
         slug: {
             type: String,
             lowercase: true,
-            unique: true,
+            required: true,
             index: true
-        },
-        image: {
-            url: String,
-            key: String
-        },
-        content: {
-            type: {},
-            min: 20,
-            max: 2000000
         },
         postedBy: {
             type: ObjectId,
             ref: 'User'
-        }
+        },
+        categories: [
+            {
+                type: ObjectId,
+                ref: 'Category',
+                required: true
+            }
+        ],
+        type: {
+            type: String,
+            default: 'Free'
+        },
+        medium: {
+            type: String,
+            default: 'Video'
+        },
+        clikes: { type: Number, default: 0 }
     },
     { timestamps: true }
 );
 
-module.exports = mongoose.model('Category', categorySchema);
+module.exports = mongoose.model('Link', linkSchema);
