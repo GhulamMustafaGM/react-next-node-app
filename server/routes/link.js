7a84921222ce51b961914cf9025bfd060c187faf
Ghulam Mustafa <mustafaji@gmail.com>
2021-11-04 21:19:29 +0100

    updated link routes

--- server/routes/link.js
+++ server/routes/link.js
@@ -14,7 +14,7 @@ router.post('/link', linkCreateValidator, runValidation, requireSignin, authMidd
 router.get('/links', list);
 router.put('/click-count', clickCount);
 router.get('/link/:slug', read);
-router.put('/link/:slug', linkUpdateValidator, runValidation, requireSignin, authMiddleware, create);
+router.put('/link/:id', linkUpdateValidator, runValidation, requireSignin, authMiddleware, update);
 router.delete('/link/:id', requireSignin, authMiddleware, remove);
 
 module.exports = router;
