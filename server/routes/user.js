a0f6c656f46064966725427a8218eed508c3fda5
Ghulam Mustafa <mustafaji@gmail.com>
2021-12-02 19:47:05 +0100

    updated user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,29 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // import middlewares
-const {
-  requireSignin,
-  authMiddleware,
-  adminMiddleware,
-} = require("../controllers/auth");
+const { requireSignin, authMiddleware, adminMiddleware } = require('../controllers/auth');
 // import validator
-const { userUpdateValidator } = require("../validators/auth");
-const { runValidation } = require("../validators");
+const { userUpdateValidator } = require('../validators/auth');
+const { runValidation } = require('../validators');
 
 // import controllers
-const { read, update } = require("../controllers/user");
+const { read, update } = require('../controllers/user');
 
 // routes
-router.get("/user", requireSignin, authMiddleware, read);
-router.get("/admin", requireSignin, adminMiddleware, read);
-router.put(
-  "/user",
-  userUpdateValidator,
-  runValidation,
-  requireSignin,
-  authMiddleware,
-  update
-);
+router.get('/user', requireSignin, authMiddleware, read);
+router.get('/admin', requireSignin, adminMiddleware, read);
+router.put('/user', userUpdateValidator, runValidation, requireSignin, authMiddleware, update);
 
 module.exports = router;
